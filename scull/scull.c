97ed354478a49f63155f30a5b27f1e7db4e06a28
Steven Kang <kangweibaby@163.com>
2021-06-16 17:27:09 +0800

    update scull

--- scull/scull.c
+++ scull/scull.c
@@ -1,17 +1,29 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/types.h>
+#include <linux/fs.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+static dev_t dev_first;
+static const u32 dev_count = 1;
+static const char *module_name = "scull";
+
 static int scull_init(void)
 {
-    printk(KERN_ALERT "hello, scull!!!\n");
+    printk(KERN_ALERT "hello, start initialize scull!!!\n");
+
+    alloc_chrdev_region(&dev_first, 0, dev_count, module_name);
+
+    file_operations fo;
+
     return 0;
 }
 
 static void scull_exit(void)
 {
     printk(KERN_ALERT "bye, scull!!!\n");
+    unregister_chrdev_region(dev_first, dev_count);
 }
 
 module_init(scull_init);
